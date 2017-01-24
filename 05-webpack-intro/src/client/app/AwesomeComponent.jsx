4a98c82f00f6c2d065f8890196cc3f817762700e
Luca Hagel <hagel.luca@gmail.com>
2017-01-24 12:42:35 -0800

    05 Webpack Tutorial

---
+++ 05-webpack-intro/src/client/app/AwesomeComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class AwseomeComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      likesCount: 0,
+    };
+    this.onLike = this.onLike.bind(this);
+  }
+
+  onLike() {
+    this.setState({likesCount: this.state.likesCount + 1});
+  }
+  render() {
+    return (
+      <div>
+        Likes: <span>{this.state.likesCount}</span>
+        <div><button onClick={this.onLike}>Like</button></div>
+      </div>
+    );
+  }
+}
